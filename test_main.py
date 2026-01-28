18fa98492383dd1055442c10a74aeb6034716728
zentrix566 <zentrix566@gmail.com>
2026-01-28 16:28:48 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,8 +1,2 @@
-from main import add
-
-print("--- 开始自动化测试 ---")
-
-# 如果 add(1, 1) 的结果不是 2，程序会在这里“炸掉”并报错
-assert add(1, 1) == 2, f"错误：期待结果为 2，但实际得到的是 {add(1,1)}"
-
-print("--- 测试通过！ ---")
+def test_add():  
+    assert add(1, 1) == 2
